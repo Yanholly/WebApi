70bda5a49cb6d9977175cf75dd4f6f890b5dc9db
Yanholly <m9khamets@gmail.com>
2019-01-13 01:42:27 +0300

    first version

---
+++ TestApi/Controllers/SendMsgController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NPushover;
+namespace TestApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendMsgController : ControllerBase
+    {   
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Models.MessageInfo value)
+        {
+            var po = new Pushover(value.app_token);
+            var msg = NPushover.RequestObjects.Message.Create(value.msg_text);
+            var sendtask = po.SendMessageAsync(msg, value.user_key);
+        }
+    }
+}
---
+++ TestApi/Models/MessageInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApi.Models
+{
+    public class MessageInfo
+    {
+  
+        public string app_token { get; set; }
+
+        public string user_key { get; set; }
+
+        public string msg_text { get; set; }
+
+
+    }
+}
